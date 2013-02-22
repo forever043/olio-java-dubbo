ea2b3ce4de55306588318aab5c2bc5342221346f
MaJiuyue <majiuyue@ncic.ac.cn>
2013-02-22 09:32:12 +0800

    add un-offical jar install script

---
+++ contrib/install-jar.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -DgroupId=jmaki -DartifactId=ajax-wrapper-comp -Dversion=1.8.1 -Dpackaging=jar -DgeneratePom=true -Dfile=ajax-wrapper-comp-1.8.1.jar
+mvn install:install-file -DgroupId=com.danga -DartifactId=memcached -Dversion=2.0.1 -Dpackaging=jar -DgeneratePom=true -Dfile=java_memcached-release_2.0.1.jar
+mvn install:install-file -DgroupId=org.json -DartifactId=json -Dversion=no.ver -Dpackaging=jar -DgeneratePom=true -Dfile=JSON.jar
